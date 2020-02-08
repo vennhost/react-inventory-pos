f25aa2b0725ca9095f93d7b3c7586d584e7c3c4a
vennhost <samogconsult@gmail.com>
2020-02-08 22:06:39 +0100

    Fixed modules

--- react-pos/src/index.js
+++ react-pos/src/index.js
@@ -4,11 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { makeRoutes } from "./routes";
+//import { makeRoutes } from "./routes";
 import App from "./App";
 //import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

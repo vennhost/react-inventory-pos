54d2913af44fa492ea94445fdddbf06e78a11c33
vennhost <samogconsult@gmail.com>
2020-02-08 21:35:50 +0100

    Components

--- react-pos/src/index.js
+++ react-pos/src/index.js
@@ -6,11 +6,11 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { makeRoutes } from "./routes";
 import App from "./App";
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+registerServiceWorker();
